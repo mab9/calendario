782e60cda223de67e977f474605366be68e94eb0
marc-antoine <marcantoine.bruelhart@gmail.com>
2021-12-12 12:36:10 +0100

    Add js client calendar dummy html events

--- js-client/src/cali/cali.js
+++ js-client/src/cali/cali.js
@@ -8,10 +8,14 @@ export {CaliController, CaliView};
  */
 const CaliController = () => {
 
+    const create = from => to => alert("new event from " + from  + " to " + to + ".");
+
     /**
      * @typedef {Readonly<object>} CaliController
      */
-    return Object.freeze({})
+    return Object.freeze({
+        create: create
+    })
 }
 
 /**
@@ -21,14 +25,39 @@ const CaliController = () => {
  */
 const CaliView = (rootElement, caliController) => {
 
-    const render = () => {
-        const layoutElement = dom(`
+    const view = dom(`
             <DIV id="content-section"></DIV>
-                    I'm in the main content.
+                    <div>
+                        <button id="create">+</button>
+                        <div id="event-1" class="card">
+                            <div>from 2021-12-03</div>
+                            <div>to 2021-12-12</div>
+                            <div>10 days in a row</div>
+                            <div>6 holidays</div>
+                            <div>Status: <strong>approved</strong></div>
+                        </div>
+
+                        <div id="event-2" class="card">
+                            <div>from 2021-12-23</div>
+                            <div>to 2021-12-28</div>
+                            <div>5 days in a row</div>
+                            <div>3 holidays</div>
+                            <div>Status: <strong>requested</strong></div>
+                        </div>
+
+                        <div id="event-2" class="card">
+                            <div>from 2022-02-12</div>
+                            <div>to 2022-02-15</div>
+                            <div>4 days in a row</div>
+                            <div>4 holidays</div>
+                            <div>Status: <strong>denied</strong></div>
+                        </div>
+
+                    </div>
             </DIV>`);
 
-        rootElement.appendChild(layoutElement)
-    };
+    const createBtn = view.querySelector('#create');
+    createBtn.onclick = () => caliController.create("1")("2");
 
-    render();
+    rootElement.appendChild(view)
 };
