c4045c455a40943ae01b4f421e182474553a8877
marc-antoine <marcantoine.bruelhart@gmail.com>
2021-12-12 18:15:49 +0100

    js-client implement projector

--- js-client/src/event/event.js
+++ js-client/src/event/event.js
@@ -2,34 +2,35 @@ import {dom} from "../base/church/dom.js";
 import {Observable, ObservableList} from '../base/observable/observable.js';
 import {
     Attribute,
-    LABEL,
-    valueOf,
-    setValueOf,
     setLabelOf,
 } from '../base/presentationModel/presentationModel.js';
+import {eventListItemProjector} from './event.projector.js';
 
 export {MasterController, EventView, SelectionController};
 
+/**
+ * @return Event
+ * @constructor
+ */
 const Event = () => {                               // facade
     const from = Attribute(new Date());
     setLabelOf(from)("From");
 
-    const to  = Attribute(new Date());
+    const to = Attribute(new Date());
     setLabelOf(to)("To");
 
-    const status  = Attribute("requested");
-    setLabelOf(status)("Status");
+    const state = Attribute("requested");
+    setLabelOf(state)("State");
 
     // xyzAttr.setConverter( input => input.toUpperCase() );
     // xyzAttr.setValidator( input => input.length >= 3   );
 
     return {
-        from:   from,
-        to:     to,
-        status: status,
+        from: from,
+        to: to,
+        state: state,
     }
 };
-
 
 /**
  * @return MasterController
@@ -38,9 +39,6 @@ const Event = () => {                               // facade
 const MasterController = () => {
 
     const itemListModel = ObservableList([]); // observable array of events, this state is private
-
-    const create = from => to => alert(
-        "new event from " + from + " to " + to + ".");
 
     return {
         addItem:      () => itemListModel.add(Event()),
@@ -64,23 +62,17 @@ const EventView = (masterController, selectionController, rootElement) => {
             <div id="event-1" class="card">
                 <div>from 2021-12-03</div>
                 <div>to 2021-12-12</div>
-                <div>10 days in a row</div>
-                <div>6 holidays</div>
-                <div>Status: <strong>approved</strong></div>
+                <div>state: <strong>approved</strong></div>
             </div>
             <div id="event-2" class="card">
                 <div>from 2021-12-23</div>
                 <div>to 2021-12-28</div>
-                <div>5 days in a row</div>
-                <div>3 holidays</div>
-                <div>Status: <strong>requested</strong></div>
+                <div>state: <strong>requested</strong></div>
             </div>
             <div id="event-2" class="card">
                 <div>from 2022-02-12</div>
                 <div>to 2022-02-15</div>
-                <div>4 days in a row</div>
-                <div>4 holidays</div>
-                <div>Status: <strong>denied</strong></div>
+                <div>state: <strong>denied</strong></div>
             </div>
         </div>
     `);
@@ -90,21 +82,13 @@ const EventView = (masterController, selectionController, rootElement) => {
 
     const eventsContainer = view.querySelector('.events');
 
-    masterController.onItemAdd(item => {
-        eventsContainer.appendChild(dom(`
-             <div id="event-2" class="card">
-                <div>from ` + valueOf(item.from).toISOString().substring(0, 10) + `</div>
-                <div>to ` + valueOf(item.to).toISOString().substring(0, 10) + `</div>
-                <div>4 days in a row</div>
-                <div>4 holidays</div>
-                <div>Status: <strong>` + valueOf(item.status) + `</strong></div>
-            </div>
-        `));
-    })
+    const render = item => eventListItemProjector(masterController, selectionController, eventsContainer, item);
+
+    // binding
+    masterController.onItemAdd(render);
 
     rootElement.appendChild(view)
 };
-
 
 /**
  * @return SelectionController
@@ -112,14 +96,14 @@ const EventView = (masterController, selectionController, rootElement) => {
  */
 const SelectionController = () => {
 
-    const selectedPersonObs = Observable("NoPerson");
+    const selectedItem = Observable("NoItem");
     return {}
 
     /*
     return {
-        setSelectedPerson : selectedPersonObs.setValue,
-        getSelectedPerson : selectedPersonObs.getValue,
-        onPersonSelected:   selectedPersonObs.onChange,
-        clearSelection:     () => selectedPersonObs.setValue(NoPerson),
+        setSelectedItem : selectedItemObs.setValue,
+        getSelectedItem : selectedItemObs.getValue,
+        onItemSelected:   selectedItemObs.onChange,
+        clearSelection:     () => selectedItemObs.setValue("NoItem"),
     }*/
 };
