523a2c8917590ce1a0c89c2749ec7e6762e01402
marc-antoine <marcantoine.bruelhart@gmail.com>
2021-12-27 12:55:32 +0100

    js-client update local test data

--- js-client-iteration-2/src/service/event.service.local.js
+++ js-client-iteration-2/src/service/event.service.local.js
@@ -9,7 +9,7 @@ const vakansieService = () => {
     /** @type Event[] */
     const events = [
         {id: 0, from: '2021-12-20', to: '2021-12-24', state: 'accepted'},
-        {id: 1, from: '2021-12-31', to: '2022-01-15', state: 'requested'},
+        {id: 1, from: '2022-01-11', to: '2022-01-15', state: 'requested'},
         {id: 2, from: '2022-04-12', to: '2022-04-17', state: 'requested'},
     ];
 
