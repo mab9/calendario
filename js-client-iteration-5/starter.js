7b4b7b05e6f506aaa7b3d1c2222746c3a87305fc
marc-antoine <marcantoine.bruelhart@gmail.com>
2022-01-01 19:18:46 +0100

    jci-5 - separate event specific code from starter.js

--- js-client-iteration-5/starter.js
+++ js-client-iteration-5/starter.js
@@ -1,9 +1,8 @@
-import {ServiceController} from './src/service/service.controller.js';
 import {translationService} from './src/base/service/translation.service.js';
 import {child, dom} from './src/base/utils/dom.js';
-import {EventView, EventController, OverView} from './src/event/event.js';
-import {SelectionController} from './src/base/controller/controller.js';
 import {setValueOf} from './src/base/presentationModel/presentationModel.js';
+import {EventView} from './src/event/event.view.js';
+import {EventController} from './src/event/event.controller.js';
 
 import {config} from './config.js';
 
@@ -12,7 +11,7 @@ import {config} from './config.js';
  * @param {Event[]} events
  * @constructor
  */
-const start = (appRootIdParam, events) => {
+const start = (appRootIdParam) => {
 
     translationService.init();
 
@@ -27,28 +26,18 @@ const start = (appRootIdParam, events) => {
 
                 <div class="topnav-languages"></div>
             </NAV>
-            <DIV id="content">
-                <button class="plus-btn">+</button>
-                <div class="overview"></div>
-                <div class="events"></div>
-            </div>
+            <DIV id="content"></div>
         </div>`
     );
 
     const mainContainer = child(fragment);
+
     const eventCtrl = EventController();
-    const selectionController = SelectionController("NoItem");
     const content = mainContainer.querySelector("#content");
-    const languages = mainContainer.querySelector(".topnav-languages");
-
-    const [createBtn, overViewContainer, eventContainer] = content.children;
-
-    OverView(eventCtrl, selectionController, overViewContainer);
-    EventView(eventCtrl, selectionController, eventContainer);
-
-    createBtn.onclick = () => eventCtrl.createItem();
+    EventView(eventCtrl, content);
 
     // translations
+    const languages = mainContainer.querySelector(".topnav-languages");
     const currentLang = translationService.currentLang;
     config.languages.forEach(lang => {
         const langElement = dom(`<a>${lang}</a>`);
@@ -56,10 +45,9 @@ const start = (appRootIdParam, events) => {
         languages.append(langElement)
     })
 
-    events.forEach(item => eventCtrl.processNewModel(item));
     root.replaceWith(mainContainer); // why replace???
 }
 
 const appRootId = window.appRootId;
 
-ServiceController().vakansieService.loadeEvents(events => start(appRootId, events));
+start(appRootId);
