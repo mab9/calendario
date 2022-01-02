3f8a895063b3b07d4b7c5427df769286a3a6ac3f
marc-antoine <marcantoine.bruelhart@gmail.com>
2022-01-02 17:14:46 +0100

    jci-5 - prepare router test

--- js-client-iteration-5/src/base/router/router.test.js
+++ js-client-iteration-5/src/base/router/router.test.js
@@ -1,9 +1,29 @@
 import {Suite} from "../test/test.js"
+import {dom} from '../utils/dom.js';
 
 const suite = Suite("router");
 
-suite.add("empty test", assert => {
-    assert.is(5, 5);
+const getContainerChildsById = containerId => document.querySelector(`#${containerId}`).children;
+
+
+suite.add("setup test", assert => {
+    // setup
+    const containerId = 'router-test';
+
+    const fragment = dom(`
+        <div id="${containerId}">
+            <a href="/home">Home</a>
+            <a href="/calendar">Calendar</a>
+            <div id="app">routing content</div>
+        </div>`
+    )
+
+    const body = document.querySelector('body');
+    body.appendChild(fragment);
+
+    const [home, calendar, content] = getContainerChildsById(containerId);
+
+    assert.is(content.innerHTML, 'routing content');
 });
 
 suite.run();
