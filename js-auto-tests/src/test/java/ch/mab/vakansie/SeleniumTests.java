bbe14cfa29a9b810bc293c0eadb82b31aedb5989
marc-antoine <marcantoine.bruelhart@gmail.com>
2022-01-30 19:47:57 +0100

    jci-6 - automate frontend test with pre build docker image

--- js-auto-tests/src/test/java/ch/mab/vakansie/SeleniumTests.java
+++ js-auto-tests/src/test/java/ch/mab/vakansie/SeleniumTests.java
@@ -13,12 +13,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
+@Testcontainers
 class SeleniumTests {
+
 
     // https://medium.com/@milosz.wozniak/how-to-setup-selenium-java-with-gradle-minimum-dependencies-c557b19c1967
     // test report : build/reports/tests/test/index.html
-    private WebDriver driver;
+    private static WebDriver BROWSER;
+
+    // In situations where there is no pre-existing Docker image,
+    // Testcontainers can create a new temporary image on-the-fly from a Dockerfile.
+    // https://www.testcontainers.org/features/creating_images/
+    @Container
+    private static final GenericContainer FRONTEND_CONTAINER = new GenericContainer(DockerImageName.parse("lighttpd")).withExposedPorts(3000);
+    private static String FRONTEND_URL;
 
     @BeforeAll
     public static void setupWebdriverFirefoxDriver() {
@@ -27,25 +40,32 @@ class SeleniumTests {
 
     @BeforeEach
     public void setup() {
-        driver = new FirefoxDriver();
+        BROWSER = new FirefoxDriver();
+
+        String host = FRONTEND_CONTAINER.getHost();
+        Integer port = FRONTEND_CONTAINER.getFirstMappedPort();
+        FRONTEND_URL = String.format("http://%s:%d/test.html", host, port);
     }
 
     @AfterEach
     public void teardown() {
-        if (driver != null) {
-            driver.quit();
+        if (BROWSER != null) {
+            BROWSER.quit();
         }
     }
 
     @Test
     void verifyVakansieTests() {
-        driver.get("http://localhost:3000/test.html");
-        WebElement total = driver.findElement(By.id("grossTotal"));
+        BROWSER.get(FRONTEND_URL);
+        WebElement total = BROWSER.findElement(By.id("grossTotal"));
+
         assertThat(total, notNullValue());
 
-        // When tests were successful, we expect a number followed by a string
+        // When test.html tests were successful, we expect a number followed by a string
         String amountOfSuccessfulTests = total.getText().split(" ")[0];
         assertThat(total.getText(), endsWith("Tests"));
-        assertThat(Integer.parseInt(amountOfSuccessfulTests), greaterThan(140)); // there are roughly 140 tests at the moment.
+
+        // there are roughly 140 tests at the moment.
+        assertThat(Integer.parseInt(amountOfSuccessfulTests), greaterThan(140));
     }
 }
